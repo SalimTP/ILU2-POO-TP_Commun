3c846254783d6c4069853b31e13d487a96098bee
SalimTP <mss4170a@ens.fsi>
2023-04-06 19:30:15 +0200

    TP COMMUN

--- src/model/CentraleReservation.java
+++ src/model/CentraleReservation.java
@@ -1,7 +1,39 @@
 package model;
 
 public class  CentraleReservation <E extends EntiteReservable <F>, F extends Formulaire>  {
+    private E[] entites;
+    int nbEntites;
+    
+    public CentraleReservation(E[] entites) {
+        this.entites = entites;
+     }
+       
+    public void ajouterEntite(E entite) {
+        entites[nbEntites] = entite; 
+        nbEntites++;        
+    } 
+    
+    public int[] donnerPossibilites(F formulaire) {
+        int[] valeurs = new int[nbEntites];
+        for (int i = 0; i < this.nbEntites; i++) {
+            if (entites[i].compatible(formulaire) && this.entites[i].estLibre(formulaire)) {
+                valeurs[i] = this.entites[i].getNumero();
+            } else {
+                valeurs[i] = 0;
+            }
+        }
+        return valeurs;
+    }
 
-	int nbEntite;
-	
-}
+    public Reservation reserver(int numeroEntite, F formulaire) {
+        E entite = entites[numeroEntite-1]; 
+        formulaire.setIdentificationEntite(numeroEntite); 
+        Reservation reservation = entite.reserver(formulaire); 
+        return reservation; 
+    }
+
+ }
+
+    
+
+    
